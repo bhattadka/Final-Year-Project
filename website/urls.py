22fe39f6077eb9dc17e589addb42b8f8b81bc322
Priyanshi0912 <priyanshi091202@gmail.com>
2024-08-24 18:52:50 +0530

    Till grading system

---
+++ website/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'weak_cipher_detector'
+
+urlpatterns = [
+    path('', views.analyze_certificate, name='analyze_certificate'),
+]
+
+
+
