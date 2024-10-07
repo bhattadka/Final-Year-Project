e7a5ca8ef110a90d92e9b5ccba4fffeda664f78d
bhattadka <kritikabhattad06@gmail.com>
2024-10-07 20:21:05 +0530

    Initial commit

--- wcd/wcd/settings.py
+++ wcd/wcd/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "cipher",
         "USER": "root",
-        "PASSWORD":"12345",
+        "PASSWORD":"123456@#",
         "HOST":'localhost',
         "PORT":"3306",
 
@@ -121,9 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ 
